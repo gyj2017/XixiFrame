99d00c993f07aa96612afcb61a828755dd7e927b
xixihaha <757493555@qq.com>
2023-05-09 22:48:16 +0800

    项目初始化

---
+++ api/src/main/java/com/gyj/api/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.gyj.api.controller;
+
+import com.gyj.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private UserService userService;
+
+
+
+}
---
+++ api/src/main/java/com/gyj/api/domain/User.java
@@ -0,0 +1,80 @@
+package com.gyj.api.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户表
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 主键ID
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 
+     */
+    @TableField(value = "username")
+    private String username;
+
+    /**
+     * 
+     */
+    @TableField(value = "password")
+    private String password;
+
+    /**
+     * 
+     */
+    @TableField(value = "avatar")
+    private String avatar;
+
+    /**
+     * 
+     */
+    @TableField(value = "phone")
+    private String phone;
+
+    /**
+     * 
+     */
+    @TableField(value = "login_date")
+    private Date loginDate;
+
+    /**
+     * 
+     */
+    @TableField(value = "status")
+    private Integer status;
+
+    /**
+     * 
+     */
+    @TableField(value = "create_time")
+    private Date createTime;
+
+    /**
+     * 
+     */
+    @TableField(value = "update_time")
+    private Date updateTime;
+
+    /**
+     * 
+     */
+    @TableField(value = "remark")
+    private String remark;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
